5d7a563ffd01297003806df276faefea989d0835
Tim Digel <tim.digel@active-group.de>
2019-06-27 11:02:43 +0200

    Add Travis-CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule QuickStruct.MixProject do
     [
       app: :quick_struct,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
